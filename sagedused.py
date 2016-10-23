1ed97a1bdc9eaa5a2d9cda36d084c50c357214eb
Märt Põder <boamaod@gmail.com>
2016-10-23 08:31:32 +0300

    Detect most used words.

--- sagedused.py
+++ sagedused.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, string
+import re, sys, string
 from pyvabamorf import analyze
 from pprint import pprint
 
-words={}
-table = string.maketrans("","")
+import urllib2, codecs
 
 IGN_TYYP = ['J', 'Z', 'D', 'P'] # sidesõna, lausemärk, operaator, küsisõna
 IGN_WORD = ["olema"]
 
-def ins_or_add(analysis):
+def ins_or_add(analysis, words):
 	word = analysis['lemma']
 	tyyp = analysis['partofspeech']
 	#word=word+"_"+tyyp
@@ -21,18 +20,57 @@ def ins_or_add(analysis):
 		else:
 			words[word] = 1
 
+def dl_doc(url):
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    return response.read()
+
+def strip_html(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+
+def get_content(url):
+	resp = dl_doc(url)
+	content = strip_html(resp)
+	return content
+
+def get_freq(data):
+	words={}
+
+	for word in data.split():
+		word = word.lower()
+		word = filter(unicode.isalpha, unicode(word, "utf-8"))
+		if len(word) < 3:
+			continue;
+		res = analyze(word)
+		ins_or_add(res[0]['analysis'][0], words)
+
+	return sorted(words.items(), key=lambda x:x[1], reverse=True)
+
 if len(sys.argv) >= 2:
 	with open(sys.argv[1]) as f:
-		data = f.readlines()
+		data_file = f.readlines()
 
-	for line in data:
-		for word in line.split():
-			word.translate(table, string.punctuation)
-			res = analyze(word)
-			ins_or_add(res[0]['analysis'][0])
+output=sys.argv[1].split(".")[0]+"-tags.csv"
+fw = codecs.open(output,'w','utf-8')
 
-words = sorted(words.items(), key=lambda x:x[1], reverse=True)
+print output
 
-pprint(words)
+for line1 in data_file:
+	url = line1.split()[0].strip('"')
+	#print url
+	if len(url)>15:
+		data = get_content(url)
+		aaa = get_freq(data)[0:20]
+		tags = []
+		for zzz in aaa:
+			tags.append(zzz[0])
+		line = "\""+url+"\",\"" + " ".join(tags) + "\""
+		print line
+		fw.write(line + "\n")
+
+fw.close()
+
+#pprint(words)
 
 
